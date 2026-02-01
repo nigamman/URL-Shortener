1364c1d86cdcadb3bf92af8e8c3db3c4cad41e4a
Shivansh Nigam <shivanshnigam07@gmail.com>
2026-02-01 17:46:12 +0530

    Implement URL shortener functionality with encoding and decoding features

--- src/URLShortener.h
+++ src/URLShortener.h
@@ -1,4 +1,26 @@
-int main() {
-    cout << hello << endl;
-    return 0;
-}
+#ifndef URL_SHORTENER_H
+#define URL_SHORTENER_H
+
+#include <unordered_map>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+class UrlShortener {
+private:
+    long long idCounter;
+    unordered_map<long long, string> idToUrl;
+    unordered_map<string, long long> shortToId;
+    unordered_map<long long, time_t> expiry;
+
+    string base62Encode(long long id);
+    bool isExpired(long long id);
+
+public:
+    UrlShortener();
+    string encode(const string& longUrl, int ttl = -1);
+    string decode(const string& shortUrl);
+};
+
+#endif
---
+++ src/UrlShortener.cpp
@@ -0,0 +1,56 @@
+#include "UrlShortener.h"
+#include <iostream>
+#include <algorithm>
+
+static const string BASE62 = 
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+UrlShortener::UrlShortener() {
+    idCounter = 1; // start from 1 for clarity
+}
+
+string UrlShortener::base62Encode(long long id) {
+    string encoded;
+    while (id > 0) {
+        encoded.push_back(BASE62[id % 62]);
+        id /= 62;
+    }
+    reverse(encoded.begin(), encoded.end());
+    return encoded;
+}
+
+bool UrlShortener::isExpired(long long id) {
+    if (expiry.find(id) == expiry.end()) return false;
+    return time(nullptr) > expiry[id];
+}
+
+string UrlShortener::encode(const string& longUrl, int ttl) {
+    long long id = idCounter++;
+    string shortCode = base62Encode(id);
+
+    idToUrl[id] = longUrl;
+    shortToId[shortCode] = id;
+
+    if (ttl != -1) {
+        expiry[id] = time(nullptr) + ttl;
+    }
+
+    return shortCode;
+}
+
+string UrlShortener::decode(const string& shortUrl) {
+    if (shortToId.find(shortUrl) == shortToId.end()) {
+        return "URL not found";
+    }
+
+    long long id = shortToId[shortUrl];
+
+    if (isExpired(id)) {
+        idToUrl.erase(id);
+        shortToId.erase(shortUrl);
+        expiry.erase(id);
+        return "URL expired";
+    }
+
+    return idToUrl[id];
+}
